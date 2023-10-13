20ffe5dd2b5d8d2e2178a9ca5716d419f8380179
thaehae <tmhaehae@gmail.com>
2023-10-13 17:48:17 +1300

    lesson05 Book of Mormon activity

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,21 @@
+const input = document.querySelector("#input");
+
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function () {
+    //if input.value !='hello';
+    '';
+});
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❎';
+li.append(deleteButton);
+list.append(li);
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+input.focus;
+input.value = ('');
