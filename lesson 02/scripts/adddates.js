5586d62bd06051b2e46f7b10e76ddda288eca23d
thaehae <tmhaehae@gmail.com>
2023-10-20 16:08:41 +1300

    deleted files images favicons mqueries etc

--- lesson 02/scripts/adddates.js
+++
@@ -1,7 +0,0 @@
-const dateYear = document.querySelector(".date");
-const year = new Date().getFullYear();
-dateYear.textContent = year
-
-const modifyDate = document.querySelector(".modify");
-const mod = new Date(document.lastModified).toLocaleString();
-modifyDate.textContent = mod
